388911584b40e7ee703419ee53f3f82f5e836fad
Sathwik Gaddi <sathwik.gaddi@wipro.com>
2023-07-11 12:18:24 -0400

    Updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -102,23 +102,7 @@
     offset: '80%'
   });
 
-  
-  // $(".testimonials-carousel").owlCarousel({
-  //   autoplay: true,
-  //   dots: true,
-  //   loop: true,
-  //   responsive: {
-  //     0: {
-  //       items: 1
-  //     },
-  //     768: {
-  //       items: 2
-  //     },
-  //     900: {
-  //       items: 3
-  //     }
-  //   }
-  // });
+
 
  
   $(window).on('load', function() {
